2e69e5e7f4fd9d0bd91ffad3d8cd95231b6527c5
Doug McGeehan <djmvfb@mst.edu>
2015-02-08 17:07:02 -0600

    now detecting the proper number of items

--- cv.py
+++ cv.py
@@ -26,22 +26,12 @@ class SimplePlot:
     self.n += 1
 
 
-def to_monochrome(img):
-  # Otsu's thresholding after Gaussian filtering
-  blur = cv2.GaussianBlur(img, (5,5), 0)
-  _, threshold = cv2.threshold(
-    blur, 0, 255,
-    cv2.THRESH_BINARY+cv2.THRESH_OTSU
-  )
-
-  opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, (5, 5))
-  closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, (5, 5))
-
-  return threshold
-  
-
 if __name__ == "__main__":
-  img = cv2.imread(sys.argv[1])
+  border_reduction = 50
+  img = cv2.imread(sys.argv[1])[
+    border_reduction:-border_reduction,
+    border_reduction:-border_reduction
+  ]
   gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   cv2.imwrite("01_gray.png", gray_img)
 
@@ -54,15 +44,19 @@ if __name__ == "__main__":
   )
   cv2.imwrite("03_threshold.png", threshold)
 
-  kernel = np.ones((5,5),np.uint8)
+  kernel = np.ones((8, 8),np.uint8)
   opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
   cv2.imwrite("04_opening.png", opening)
 
   closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
   cv2.imwrite("05_closing.png", closing)
 
-  closing_inv = cv2.bitwise_not(closing)
-  contours, hierarchy = cv2.findContours(closing_inv, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  negated = cv2.bitwise_not(closing)
+  cv2.imwrite("05__negated.png", negated)
+
+  contours, hierarchy = cv2.findContours(
+    negated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+  )
   blank_image_contours = np.zeros(img.shape, np.uint8)
   blank_image = np.zeros(img.shape, np.uint8)
   next_countour_indices = hierarchy[0,:,0]
@@ -74,9 +68,10 @@ if __name__ == "__main__":
     cv2.drawContours(blank_image_contours, c, -1, (255, 0, 0), 5)
     x,y,w,h = cv2.boundingRect(c)
 
-    print("Size: {0}  | ".format(w*h), (x,y), (x+w, y+h))
+    print("Size: {0}  | ".format(w*h), (x,y), (x+w, y+h), (w, h))
     cv2.rectangle(blank_image, (x,y), (x+w,y+h), (r(), r(), r()), 10)
     next_index = next_countour_indices[next_index]
 
+  print("Number of detected objects: {0}".format(i))
   cv2.imwrite("06_contours.png", blank_image_contours)
   cv2.imwrite("07_bounding_boxes.png", blank_image)
